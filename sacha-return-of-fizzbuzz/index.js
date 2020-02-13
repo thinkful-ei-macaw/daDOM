c7f552b1ea81d3878044731ab2925655bbd253c8
Sacha Sedriks <sachased@gmail.com>
2020-02-12 23:02:14 -0800

    Save latest before calling it a night. Exercise fizzbuzz incomplete. For some reason annoyingly have /.vscode/settings.json added, too... no idea why.

--- sacha-return-of-fizzbuzz/index.js
+++ sacha-return-of-fizzbuzz/index.js
@@ -1,1 +1,39 @@
 'use strict';
+
+function results() {
+
+  $('#number-chooser').submit(event => {
+    
+    // stop default form submission behaviour
+    event.preventDefault();
+
+    // get number submitted
+    //  parseInt() function parses a string argument and returns an integer
+    const num = parseInt( $(event.currentTarget).find('input[name="number-choice"]').val());
+    console.log(num);
+
+
+    // insert <div> element with class selector
+    $('.js-results').addClass('fizz-buzz-item');
+
+    // set up conditional for results
+    if (num >= 0) {
+      if (num % 3 === 0 && num % 5 === 0) {
+        //fizzbuzz
+      } else if (num % 3 === 0) {
+        //fizz
+      } else if (num % 5 === 0) {
+        //buzz
+      } else {
+        //display number
+      }
+    } else {
+      // null, not a positive number
+    }
+
+
+    // console.log('listener firing!!');
+    // console.log(event.target);
+  });
+}
+$(results);
